d8cacaf4e5b7b256654a5e1d84ae51bd3dfffa24
hariesramdhani <hydrolizedmaltose@gmail.com>
2024-06-10 22:28:46 +0100

    Fixed baseUrl config

---
+++ assets/js/ea94b387.95bfb5bb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmorganlab_wiki=self.webpackChunkmorganlab_wiki||[]).push([[936],{6716:a=>{a.exports=JSON.parse('{"tags":[{"label":"maxwell","permalink":"/wiki/tags/maxwell","count":1},{"label":"hpc","permalink":"/wiki/tags/hpc","count":1}]}')}}]);
